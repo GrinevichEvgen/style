f3609577ec26e668fbdf06a07dd367c412d8e725
GrinevichEvgen <grinevichevgen@gmail.com>
2023-05-06 15:30:18 +0300

    dokcer install

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 import os
 
 from telegram import Update
@@ -5,13 +6,14 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         f"Hello {update.effective_user.first_name} ({update.effective_chat.id})"
     )
 
+
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
-
