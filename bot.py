430da8bc3a36c4240dca8a47fd54ab12b15a17b2
GrinevichEvgen <grinevichevgen@gmail.com>
2023-05-17 08:11:53 +0300

    server add

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 
 import os
+import logging
 
+import aioredis
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+redis = aioredis.from_url("redis://localhost")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        f"Hello {update.effective_user.first_name} ({update.effective_chat.id})"
-    )
+    logger.info(f"User {update.effective_user.first_name} - Chat ID {update.effective_chat.id}")
+    await redis.lpush("chats", update.effective_chat.id)
+    await update.message.reply_text(f"Hello, {update.effective_user.first_name}")
 
 
 if __name__ == "__main__":
--- message.py
+++ message.py
@@ -1,9 +1,10 @@
 import asyncio
 import os
-
+import aioredis
 from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+redis = aioredis.from_url("redis://localhost")
 
 
 async def send_message(chat_id: int, text: str) -> None:
@@ -11,8 +12,18 @@ async def send_message(chat_id: int, text: str) -> None:
     await bot.send_message(chat_id=chat_id, text=text)
 
 
-if __name__ == "__main__":
-    chat_id = input("Enter chat ID: ")
-    text = input("Your message: ")
-    asyncio.run(send_message(chat_id=chat_id, text=text))
+async def send_message_to_all(text: str) -> None:
+    for chat_id in {
+        await redis.lindex("chats", index)
+        for index in range(0, await redis.llen("chats"))
+    }:
+        await send_message(chat_id=int(chat_id), text=text)
 
+
+if __name__ == "__main__":
+    # chat_id = int(input("Enter chat ID: "))
+    # text = input("Your message: ")
+    # asyncio.run(send_message(chat_id=chat_id, text=text))
+
+    text = input("Your message: ")
+    asyncio.run(send_message_to_all(text=text))
